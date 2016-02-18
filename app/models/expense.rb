7d086393c71c4face6d1696789a3e298c28215c9
Wesley Soares <wesley.ssoares1@gmail.com>
2016-02-18 20:29:55 -0200

    User see total value divided by 4

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,9 @@
 class Expense < ActiveRecord::Base
   validates :description, :price, :date, presence: true
+
+  def total_divided
+    divided = 0
+    divided = price / 4
+  end
+  
 end
--- spec/factories/expenses.rb
+++ spec/factories/expenses.rb
@@ -1,4 +1,7 @@
 FactoryGirl.define do
   factory :expense do
+    date '01/02/2015'
+    description 'Combustível'
+    price 120.00
   end
 end
